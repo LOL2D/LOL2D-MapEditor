303781463f9cf122839f1f36c094058e03a16356
HoangTran <99.hoangtran@gmail.com>
2020-12-18 14:31:11 +0700

    [Update] rect block + edit block

--- main.js
+++ main.js
@@ -13,8 +13,16 @@ let setting = {
             itemPerPage: 2,
             data: TERRAIN_MAP.SUMMORNER_RIFT,
         },
+        editzone: {
+            camera: { x: 0, y: 0 },
+        },
     },
-    blocktab: {},
+    blocktab: {
+        currentBlockIndex: -1,
+        editzone: {
+            camera: { x: 0, y: 0 },
+        },
+    },
 };
 
 function setup() {
@@ -24,6 +32,9 @@ function setup() {
     stats = new Stats();
     stats.showPanel(0);
     document.body.appendChild(stats.dom);
+
+    setting.blocktab.editzone.camera.x = width / 2;
+    setting.blocktab.editzone.camera.y = height / 2;
 }
 
 function draw() {
@@ -33,7 +44,17 @@ function draw() {
 
     cursor(ARROW); // reset cursor
 
+    // body
+    if (mode == MODE.BLOCK) {
+        drawModeBlock();
+    } else {
+        drawModeMap();
+    }
+
     // header tabs
+    fill(30);
+    rect(0, 0, width, 40);
+
     if (button("Block", 5, 10, 100, 30, mode == MODE.BLOCK)) {
         mode = MODE.BLOCK;
     }
@@ -44,17 +65,27 @@ function draw() {
     fill("white");
     text("LOL2D MAP EDITOR", width / 2 + 100, 20);
 
-    // body
-    if (mode == MODE.BLOCK) {
-        drawModeBlock();
-    } else {
-        drawModeMap();
-    }
-
     // input
     pMouseIsPressed = mouseIsPressed;
+    if (keyIsDown(32)) {
+        cursor(MOVE);
+    } else {
+        cursor(ARROW);
+    }
 
     stats.end();
+}
+
+function mouseDragged() {
+    // if (keyIsDown(32)) {
+    if (mode == MODE.BLOCK) {
+        setting.blocktab.editzone.camera.x += movedX;
+        setting.blocktab.editzone.camera.y += movedY;
+    } else {
+        setting.maptab.editzone.camera.x += movedX;
+        setting.maptab.editzone.camera.y += movedY;
+    }
+    // }
 }
 
 function drawModeBlock() {
@@ -62,8 +93,12 @@ function drawModeBlock() {
     fill("#333");
     rect(0, 40, width, height - 40 - 5);
 
+    // ---------- editor zone ----------
+    drawEditBlockZone(210, 45, width - 210 - 5, height - 45 - 5);
+
     // ---------- menu zone ----------
     fill("#333");
+    noStroke();
     rect(5, 40, 200, height - 40 - 5);
 
     // buttons
@@ -90,19 +125,15 @@ function drawModeBlock() {
     if (button("Save block", 10, 200, 190, 25)) {
         console.log("save");
     }
-
-    // ---------- editor zone ----------
-    fill("gray");
-    rect(210, 45, width - 210 - 5, height - 45 - 5);
-
-    fill("white");
-    text("BLOCK", width / 2, height / 2);
 }
 
 function drawModeMap() {
     // ---------- background ----------
     fill("#333");
     rect(0, 40, width, height - 40 - 5);
+
+    // ---------- editor zone ----------
+    drawEditMapZone(210, 45, width - 210 - 5, height - 45 - 5);
 
     // ---------- menu zone ----------
     fill("#333");
@@ -134,13 +165,38 @@ function drawModeMap() {
     text("List Blocks: ", 45, 220);
 
     listScrollBlocks(10, 235, 190, 355);
+}
 
-    // ---------- editor zone ----------
-    // fill("gray");
-    // rect(210, 45, width - 210 - 5, height - 45 - 5);
+function drawEditBlockZone(x, y, w, h) {
+    fill("#555");
+    rect(x, y, w, h);
 
-    // fill("white");
-    // text("MAP", width / 2, height / 2);
+    let index = setting.blocktab.currentBlockIndex;
+    if (index >= 0) {
+        let block = setting.maptab.listBlocks.data[index];
+
+        if (block) {
+            // edit zone
+            fill("gray");
+            stroke("white");
+
+            const { camera } = setting.blocktab.editzone;
+
+            for (let r of block.rects) {
+                rect(r.x + x + camera.x, r.y + y + camera.y, r.w, r.h);
+            }
+
+            // block name
+            fill("white");
+            noStroke();
+            text(`Block: ${index} - ${block.name}`, x + w / 2, y + 10);
+        }
+    }
+}
+
+function drawEditMapZone(x, y, w, h) {
+    fill("#555");
+    rect(x, y, w, h);
 }
 
 // helpers
@@ -157,7 +213,7 @@ function listScrollBlocks(x, y, w, h) {
         if (itemIndex > 0) setting.maptab.listBlocks.itemIndex--;
     }
 
-    let isEndOfList = itemIndex == data.length - itemPerPage;
+    let isEndOfList = itemIndex >= data.length - itemPerPage;
     if (button("Scroll Down ↓", x, y + h - 20, w, 20, isEndOfList)) {
         if (!isEndOfList) setting.maptab.listBlocks.itemIndex++;
     }
@@ -165,52 +221,110 @@ function listScrollBlocks(x, y, w, h) {
     // list blocks
     let blockW = w;
     let blockH = (h - 40) / itemPerPage;
+    let index2 = min(data.length, itemIndex + itemPerPage);
 
-    for (let i = itemIndex; i < itemIndex + itemPerPage; i++) {
+    for (let i = itemIndex; i < index2; i++) {
         let blockX = x;
         let blockY = y + (i - itemIndex) * blockH + 20;
         renderBlockItem(i, data[i], blockX, blockY, blockW, blockH);
     }
 }
 
-function renderBlockItem(title, block, x, y, w, h) {
+function mouseWheel(event) {
+    // scroll list blocks
+    if (mode == MODE.MAP && isMouseInRect(10, 235, 190, 355)) {
+        const { itemIndex, itemPerPage, data } = setting.maptab.listBlocks;
+
+        if (event.delta > 0) {
+            let isEndOfList = itemIndex >= data.length - itemPerPage;
+            if (!isEndOfList) setting.maptab.listBlocks.itemIndex++;
+        } else {
+            if (itemIndex > 0) setting.maptab.listBlocks.itemIndex--;
+        }
+    }
+}
+
+function renderBlockItem(index, block, x, y, w, h) {
     // background
     stroke("white");
     noFill();
     rect(x, y, w, h);
 
     // scale up/down to fit item container
-    let maxDistance = 0;
-    for (let p of block.shapeVertices) {
-        for (let p2 of block.shapeVertices) {
-            let distance = dist(p.x, p.y, p2.x, p2.y);
-            maxDistance = max(maxDistance, distance);
-        }
+    let top = Infinity,
+        left = Infinity,
+        right = -Infinity,
+        bottom = -Infinity;
+
+    for (let p of block.rects) {
+        top = min(p.y, top);
+        bottom = max(p.y + p.h, bottom);
+        left = min(p.x, left);
+        right = max(p.x + p.w, right);
     }
 
-    let scaleRatio = min(w, h) / maxDistance;
+    let W = abs(right) - abs(left) > 0 ? abs(right) * 2 : abs(left) * 2;
+    let H = abs(bottom) - abs(top) > 0 ? abs(bottom) * 2 : abs(top) * 2;
+
+    let scaleRatio = min(w, h) / max(W, H);
 
     // draw shape
     fill("gray");
-    beginShape();
-    for (let p of block.shapeVertices) {
-        vertex(p.x * scaleRatio + x + w / 2, p.y * scaleRatio + y + h / 2);
+    for (let r of block.rects) {
+        rect(
+            r.x * scaleRatio + x + w / 2,
+            r.y * scaleRatio + y + h / 2,
+            r.w * scaleRatio,
+            r.h * scaleRatio
+        );
     }
-    endShape(CLOSE);
+
+    // draw center point
+    fill("red");
+    noStroke();
+    circle(x + w / 2, y + h / 2, 5);
 
     // title
     fill("white");
     noStroke();
-    text(title, x + 10, y + 10);
 
-    // buttons
-    if (button("Edit", x + w - 50, y + h - 20, 50, 20)) {
-        console.log("edit");
+    let title = index + " - " + block.name;
+    text(title, x + textWidth(title) / 2 + 10, y + 10);
+
+    let size = W + " x " + H;
+    text(size, w - textWidth(size) / 2, y + 10);
+
+    // on hover
+    if (isMouseInRect(x, y, w, h)) {
+        // show buttons
+        if (
+            button("Delete", x + 1, y + h - 20, 55, 20, false, "#9995", "red")
+        ) {
+            if (
+                window.confirm(
+                    `Are you sure want to delete this block index: ${index}, name: ${block.name}`
+                )
+            )
+                setting.maptab.listBlocks.data.splice(index, 1);
+        }
+        if (button("Edit", x + 56, y + h - 20, 50, 20, false, "#9995")) {
+            mode = MODE.BLOCK;
+            setting.blocktab.currentBlockIndex = index;
+        }
+        if (
+            button("Add to map →", x + 106, y + h - 20, 83, 20, false, "#9995")
+        ) {
+            console.log("add");
+        }
+
+        // show info
+        fill("white");
+        noStroke();
     }
 }
 
-function button(t, x, y, w, h, isActive) {
-    let isHover = mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
+function button(t, x, y, w, h, isActive, bgColor, hoverColor, activeColor) {
+    let isHover = isMouseInRect(x, y, w, h);
 
     // cursor
     if (isHover) {
@@ -219,11 +333,11 @@ function button(t, x, y, w, h, isActive) {
 
     // color
     if (isActive) {
-        fill("#333");
+        fill(activeColor || "#333");
     } else if (isHover) {
-        fill("#999");
+        fill(hoverColor || "#999");
     } else {
-        fill("#555");
+        fill(bgColor || "#555");
     }
     //stroke("#333");
     noStroke();
@@ -237,6 +351,10 @@ function button(t, x, y, w, h, isActive) {
     text(t, x + w / 2, y + h / 2);
 
     return isMousePressed() && isHover;
+}
+
+function isMouseInRect(x, y, w, h) {
+    return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
 }
 
 function isMousePressed() {
---
+++ map/terrain-map.js
@@ -0,0 +1,32 @@
+const TERRAIN_MAP = {
+    SUMMORNER_RIFT: [
+        {
+            name: "shape 1",
+            position: { x: 500, y: 500 },
+            rects: [
+                { x: 0, y: 0, w: 50, h: 50 },
+                { x: -20, y: -20, w: 100, h: 20 },
+            ],
+        },
+        {
+            name: "shape 2",
+            position: { x: 500, y: 500 },
+            rects: [
+                { x: -100, y: 20, w: 50, h: 50 },
+                { x: 45, y: -70, w: 100, h: 100 },
+            ],
+        },
+        {
+            name: "shape 3",
+            position: { x: 500, y: 500 },
+            rects: [
+                { x: -75, y: 20, w: 50, h: 50 },
+                { x: 20, y: 20, w: 100, h: 20 },
+                { x: 15, y: 20, w: 12, h: 20 },
+                { x: 20, y: -20, w: 20, h: 20 },
+                { x: 20, y: -50, w: 100, h: 20 },
+                { x: -20, y: 20, w: 50, h: 20 },
+            ],
+        },
+    ],
+};
